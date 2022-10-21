b617e142f625a6ab5a287524a3ec2079c564be19
kostyantyn <funyourmind@gmail.com>
2022-10-21 15:07:39 +0200

    add main.tf

---
+++ main.tf
@@ -0,0 +1,100 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Create a VPC
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    "Name" = "Terraform VPC"
+  }
+}
+
+# Create a Public subnet in AZ1
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.0.0/24"
+
+  tags = {
+    "Name" = "Public subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Public RT"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_security_group" "ssh_http_security" {
+  name        = "allow_ssh_http"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Allow SSH and HTTP"
+  }
+}
+
+resource "aws_instance" "first_instance" {
+  ami                         = "ami-08e2d37b6a0129927"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.ssh_http_security.id]
+  key_name                    = "vockey"
+  #user_data = file("script.sh")
+  tags = {
+    Name = "Amazon_Linux_2_t2_micro"
+  }
+}
