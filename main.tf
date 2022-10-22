4b0d52df2259fc6979babe94edd50894bda8da9b
kostyantyn <funyourmind@gmail.com>
2022-10-22 10:56:54 +0200

    modify security group

--- main.tf
+++ main.tf
@@ -58,22 +58,14 @@ resource "aws_route_table_association" "public_subnet_association" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_security_group" "ssh_http_security" {
-  name   = "allow_ssh_http"
+resource "aws_security_group" "ssh_security" {
+  name   = "allow_ssh"
   vpc_id = aws_vpc.myvpc.id
 
   ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -87,7 +79,7 @@ resource "aws_security_group" "ssh_http_security" {
   }
 
   tags = {
-    Name = "Allow SSH and HTTP"
+    Name = "Allow SSH"
   }
 }
 
@@ -97,7 +89,7 @@ resource "aws_instance" "first_instance" {
   associate_public_ip_address = true
   iam_instance_profile        = "LabInstanceProfile"
   subnet_id                   = aws_subnet.public_subnet.id
-  vpc_security_group_ids      = [aws_security_group.ssh_http_security.id]
+  vpc_security_group_ids      = [aws_security_group.ssh_security.id]
   key_name                    = "vockey"
   user_data                   = file("script.sh")
 
